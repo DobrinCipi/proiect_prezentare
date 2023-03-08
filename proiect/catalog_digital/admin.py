ce1abf70d1eddd82cf9b0e20ec586b547d5c65e7
Dobrin Ciprian <cipi_d08@yahoo.com>
2023-03-08 18:55:57 +0200

    Final Setup Project

--- proiect/catalog_digital/admin.py
+++ proiect/catalog_digital/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import Scoala
+
 # Register your models here.
+admin.site.register(Scoala)
--- proiect/catalog_digital/models.py
+++ proiect/catalog_digital/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Scoala(models.Model):
+    nume = models.CharField(unique=True, max_length= 33)
+    nume_director = models.CharField
+    
