785215109a2929ec3478a7900d01180bce141536
Dobrin Ciprian <cipi_d08@yahoo.com>
2023-03-12 17:24:00 +0200

    Edit admin.register for class Scoala and ClaseScoala

--- proiect/catalog_digital/admin.py
+++ proiect/catalog_digital/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 
-from .models import Scoala
+from .models import Scoala, ClaseScoala
 
 class ScoalaAdmin(admin.ModelAdmin):
     list_display = ("nume_scoala", )
     list_filter = ("nume_scoala","judet")
     search_fields = ("nume_scoala", "judet")
     
+class ClaseAdmin(admin.ModelAdmin):
+    list_display = ("scoala", "nume_clasa")
+    list_filter = ("scoala", )
+    search_fields = ("scoala", "nume_clasa", "promotia")
 
 # Register your models here.
 admin.site.register(Scoala, ScoalaAdmin)
+admin.site.register(ClaseScoala, ClaseAdmin)
--- proiect/catalog_digital/models.py
+++ proiect/catalog_digital/models.py
@@ -12,11 +12,17 @@ class Scoala(models.Model):
     oras = models.CharField(max_length=30)
     judet = models.CharField(max_length=50)
     
+    def __str__(self):
+        return f"{self.nume_scoala}"
+    
 class ClaseScoala(models.Model):
     class Meta():
         verbose_name_plural = "Clase Scoala"
        
     scoala = models.ForeignKey(Scoala, on_delete=models.CASCADE) 
-    nume_clasa = models.CharField(max_length=20)
+    nume_clasa = models.CharField(max_length=50)
     diriginte = models.CharField(max_length=20)
     promotia = models.IntegerField()
+    
+    def __str__(self):
+        return f"{self.scoala} {self.nume_clasa}"
