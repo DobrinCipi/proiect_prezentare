73d7ed34df5ee7fb9d4ca4e8c1cae5013e945301
Dobrin Ciprian <cipi_d08@yahoo.com>
2023-03-12 16:58:15 +0200

    Import Scoala class in admin

--- proiect/catalog_digital/admin.py
+++ proiect/catalog_digital/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from .models import Scoala
 
+class ScoalaAdmin(admin.ModelAdmin):
+    list_display = ("nume_scoala", )
+    list_filter = ("nume_scoala","judet")
+    search_fields = ("nume_scoala", "judet")
+    
+
 # Register your models here.
-admin.site.register(Scoala)
+admin.site.register(Scoala, ScoalaAdmin)
