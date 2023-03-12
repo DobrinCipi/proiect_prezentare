e4262aad4a0541c60d566d8a6233bc44bb7e40d3
Dobrin Ciprian <cipi_d08@yahoo.com>
2023-03-12 17:14:18 +0200

    New model "clase scoala"

--- proiect/catalog_digital/models.py
+++ proiect/catalog_digital/models.py
@@ -12,4 +12,11 @@ class Scoala(models.Model):
     oras = models.CharField(max_length=30)
     judet = models.CharField(max_length=50)
     
-    
+class ClaseScoala(models.Model):
+    class Meta():
+        verbose_name_plural = "Clase Scoala"
+       
+    scoala = models.ForeignKey(Scoala, on_delete=models.CASCADE) 
+    nume_clasa = models.CharField(max_length=20)
+    diriginte = models.CharField(max_length=20)
+    promotia = models.IntegerField()
