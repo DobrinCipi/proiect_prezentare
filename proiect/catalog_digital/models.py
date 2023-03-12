49fd5515baec4a4b80d25296fc3c68e3c8887f5f
Dobrin Ciprian <cipi_d08@yahoo.com>
2023-03-12 16:57:55 +0200

    Create Scoala class

--- proiect/catalog_digital/models.py
+++ proiect/catalog_digital/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Scoala(models.Model):
-    nume = models.CharField(unique=True, max_length= 33)
-    nume_director = models.CharField
+    
+    class Meta():
+        verbose_name_plural= "Scoli"
+        
+    nume_scoala = models.CharField(unique=True, max_length= 33)
+    nume_director = models.CharField(max_length=20)
+    adresa_scoala = models.CharField(unique=True, max_length=50)
+    judet = models.CharField(max_length=50)
+    
     
